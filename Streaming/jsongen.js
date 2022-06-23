173b8bb1005d8334f5ec8c642d7f6a8d51871c72
AndaChain <32815766+AndaChain@users.noreply.github.com>
2022-06-23 21:22:15 +0700

    Update jsongen.js

--- Streaming/jsongen.js
+++ Streaming/jsongen.js
@@ -1,5 +1,5 @@
 const { exec } = require('node:child_process');
-
+/* based on commandline to write file for show segment video have.*/
 class jsongen{
     constructor(path, manifest_path){
         exec('mse_json_manifest '+path+' > '+manifest_path+'.json', (error, stdout, stderr) => {
